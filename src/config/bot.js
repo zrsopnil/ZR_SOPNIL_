4d3c236407c6c71fbc730ae88a3b36df851d1e29
zrsopnil <zisanurrahmanshopnil@gmail.com>
2026-06-17 03:02:32 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Fuck Your MOM HARDLY",
         // Activity type number (0 = Playing).
         type: 0, 
       },
